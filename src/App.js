a6c8dc6d502b1c5836dcc4f58ef8fcf1979b175c
vishwa <kumarvishwajeettrivedi@gmail.com>
2024-02-04 19:35:20 +0530

    Latest push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-import { BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/home/Home.jsx";
 
 export default function App() {
   return (
     <div>
-    <BrowserRouter> 
-    <Main /></BrowserRouter>
-  
-   </div>
-   
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Main/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+
   );
 }
 

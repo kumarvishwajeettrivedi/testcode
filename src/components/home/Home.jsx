8522f2f9c2384da12bcc24a17575e221cbe5cef5
vishwa <kumarvishwajeettrivedi@gmail.com>
2024-02-04 18:28:29 +0530

    commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,129 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='group'>
+        <div className='wrapper'>
+          <span className='text'>Share new video</span>
+        </div>
+        <div className='box'>
+          <div className='pic' />
+          <div className='img' />
+        </div>
+        <div className='img-2' />
+        <span className='text-2'>aBit</span>
+        <div className='pic-2' />
+      </div>
+      <div className='box-2'>
+        <div className='img-3'>
+          <div className='img-4' />
+          <div className='box-3'>
+            <div className='section'>
+              <span className='text-3'>
+                Bio
+                <br />
+              </span>
+              <span className='text-4'>
+                Simply a film fan creating original content for YouTube. Let’s
+                Collaborate.
+                <br />
+              </span>
+            </div>
+            <div className='box-4'>
+              <div className='group-2'>
+                <span className='text-5'>Creator</span>
+              </div>
+              <div className='wrapper-2' />
+            </div>
+            <span className='text-6'>DAN MACE</span>
+            <span className='text-7'>/Johny_Films/</span>
+          </div>
+        </div>
+        <div className='group-3' />
+        <span className='text-8'>
+          John Bill / Creator / Moved to NYC / 27.11.22 / Live now/John Bill /
+          Creator / Moved to NYC / 27.11.22 / Live now / John Bill / Creator /
+          Moved to NYC / 27.11.22 / Live now
+        </span>
+        <div className='section-2'>
+          <div className='box-5'>
+            <div className='section-3'>
+              <div className='img-5' />
+              <span className='text-9'>
+                You haven’t uploaded any videos with aBit yet. Add now!
+              </span>
+            </div>
+            <div className='pic-3'>
+              <span className='text-a'>The Sound of Silence</span>
+              <div className='section-4'>
+                <div className='box-6'>
+                  <span className='text-b'>Raised</span>
+                  <div className='box-7'>
+                    <span className='text-c'>Shares</span>
+                  </div>
+                  <span className='text-d'>Offered</span>
+                </div>
+                <div className='box-8'>
+                  <div className='wrapper-3'>
+                    <span className='text-e'>$</span>
+                    <span className='text-f'>9510</span>
+                  </div>
+                  <span className='text-10'>317</span>
+                  <span className='text-11'>75%</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='section-5'>
+            <span className='text-12'>1</span>
+            <span className='text-13'>$9510</span>
+            <span className='text-14'>317</span>
+            <span className='text-15'>$3804</span>
+            <span className='text-16'>Releases</span>
+          </div>
+          <div className='section-6'>
+            <div className='img-6' />
+            <div className='pic-4' />
+            <div className='img-7' />
+            <div className='pic-5' />
+            <span className='text-17'>
+              Shared <br />
+              Videos
+            </span>
+            <span className='text-18'>
+              Funds
+              <br />
+              Raised
+            </span>
+            <span className='text-19'>Co-owner community</span>
+            <span className='text-1a'>
+              Co-owner
+              <br />
+              Earnings
+            </span>
+            <span className='text-1b'>
+              Videos that you upload in collaboration with aBit appear here.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className='img-8' />
+      <div className='pic-6' />
+      <div className='group-4'>
+        <span className='text-1c'>aBit</span>
+        <span className='text-1d'>Home</span>
+      </div>
+      <div className='wrapper-4'>
+        <div className='img-9' />
+        <span className='text-1e'>
+          Changing the way in which creators and fans interact.
+        </span>
+        <span className='text-1f'>Support</span>
+      </div>
+      <span className='text-20'>Are you a Creator?</span>
+    </div>
+  );
+}
